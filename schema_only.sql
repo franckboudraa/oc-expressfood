8ba57a8cfa3d1ac599f3065bb4a825343986712d
Franck Boudraa <franckboudraa@gmail.com>
2018-02-17 16:13:15 +0100

    Add files via upload

--- schema_only.sql
+++ schema_only.sql
@@ -16,64 +16,122 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `addresses`
+-- Table structure for table `addresses`
 --
 
-LOCK TABLES `addresses` WRITE;
-/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
-INSERT INTO `addresses` VALUES (1,1,'12','Avenue des Champs Elysées','75008','Paris','France','Porte 4'),(2,2,'12','Avenue des Champs Elysées','75008','Paris','France','Porte 5'),(3,3,'','Disneyland','77777','Marne-la-Vallée','France','With Minnie'),(4,6,NULL,'PO Box 57593','CA 91403','Sherman Oaks','USA',NULL),(5,1,'255','Rue de Rivoli','75001','Paris','France','At work');
-/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `addresses` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `number` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `street` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `country` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `additional_infos` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_idx` (`user_id`),
+  CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `dishes`
+-- Table structure for table `dishes`
 --
 
-LOCK TABLES `dishes` WRITE;
-/*!40000 ALTER TABLE `dishes` DISABLE KEYS */;
-INSERT INTO `dishes` VALUES (1,'Pizza','main','2018-02-10','lactose, gluten',7),(2,'Chicken Burger','main','2018-02-10','sesame, gluten',10),(3,'Cheesecake','dessert','2018-02-10','lactose',6),(4,'Apple Pie','main','2018-02-10','gluten',5);
-/*!40000 ALTER TABLE `dishes` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `dishes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dishes` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` enum('main','dessert') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cooking_date` date DEFAULT NULL,
+  `allergens` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `price` int(11) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order_items`
+-- Table structure for table `order_items`
 --
 
-LOCK TABLES `order_items` WRITE;
-/*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
-INSERT INTO `order_items` VALUES (1,2,2,10),(1,3,2,6),(2,1,1,7),(2,4,1,5),(3,1,2,7);
-/*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `order_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_items` (
+  `order_id` int(11) unsigned NOT NULL,
+  `dish_id` int(11) unsigned NOT NULL,
+  `qty` int(11) unsigned NOT NULL,
+  `price` int(11) unsigned DEFAULT NULL,
+  KEY `dish_id_idx` (`dish_id`),
+  KEY `order_id_idx` (`order_id`),
+  CONSTRAINT `dish_id` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `orders`
+-- Table structure for table `orders`
 --
 
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,1,2,'2018-02-09 13:30:40','2018-02-10 14:13:13','2018-02-09 13:50:40','2018-02-09 13:49:40','delivered',32,1),(2,3,6,'2018-02-10 14:01:39','2018-02-10 14:13:13','2018-02-10 14:21:39',NULL,'inprogress',12,3),(3,1,6,'2018-02-10 15:15:14','2018-02-10 14:18:12','2018-02-10 15:35:14',NULL,'new',14,5);
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) NOT NULL,
+  `rider_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `est_arrival_time` timestamp NULL DEFAULT NULL,
+  `arrival_time` timestamp NULL DEFAULT NULL,
+  `status` enum('new','inprogress','delivered') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `total_price` int(10) unsigned DEFAULT NULL,
+  `address_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `address_id` FOREIGN KEY (`id`) REFERENCES `addresses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `riders`
+-- Table structure for table `riders`
 --
 
-LOCK TABLES `riders` WRITE;
-/*!40000 ALTER TABLE `riders` DISABLE KEYS */;
-INSERT INTO `riders` VALUES (2,'bike'),(6,'roller');
-/*!40000 ALTER TABLE `riders` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `riders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `riders` (
+  `user_id` int(11) NOT NULL,
+  `vehicle_type` enum('bike','roller') COLLATE utf8mb4_unicode_ci DEFAULT 'bike',
+  KEY `index1` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Table structure for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Doe','John','john@doe.com','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b8','2018-02-10 11:22:42','2018-02-10 13:26:55',1,0),(2,'Doe','Samantha','samantha@doe.com','822e54d37dd37d83776ed8aac05e4578e8b201d8f3fa3','2018-02-10 12:57:43','2018-02-10 13:27:44',1,1),(3,'Mouse','Mickey','mickey@mouse.com','88f5d02acac33b8f2d1194d74431a4008f8472ae2a26d','2018-02-10 14:01:42','2018-02-10 15:18:48',1,0),(6,'Carrey','Jim','jim@carrey.com','8753ffee04e324893732bcc15ad05f4a936fe688e1f70','2018-02-10 13:38:54','2018-02-10 14:00:37',1,1);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last_name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `validated` tinyint(1) DEFAULT NULL,
+  `is_rider` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -84,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-02-17 16:11:49
+-- Dump completed on 2018-02-17 16:13:08
